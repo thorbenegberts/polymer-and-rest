7b468300c63dbca381d21aeddcf6ad4f640ec832
Thorben Egberts <thorben.egberts@plentymarkets.com>
2015-01-26 13:34:24 +0100

    refactoring: moved duplicate stuff into custom element

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,13 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+
+
+
+
+
+
+
   });
 
 // wrap document so it plays nice with other libraries
